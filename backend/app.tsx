913c705a53b2dfede56f8bfba482e2c86f52a447
AathiSakarA <aathisankara2005@gmail.com>
2025-02-16 15:35:40 +0530

    Frontend template

---
+++ backend/app.tsx
@@ -0,0 +1,16 @@
+'use strict'
+
+const { createServer } = require('node:http');
+
+const hostname = process.env.HOST;
+const port = process.env.PORT;
+
+const server = createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+  res.end('Hello World');
+});
+
+server.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`);
+});
